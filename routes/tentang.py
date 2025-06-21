efc148da702268a72c47a25eb49491cafa31f6fc
esafm24 <esa.firmansyah@gmail.com>
2025-06-21 18:35:47 +0700

    ok

--- routes/tentang.py
+++ routes/tentang.py
@@ -6,7 +6,7 @@ from Controllers.TentangManager import TentangManager
 
 tentang_view = Blueprint('tentang_routes', __name__, template_folder='/templates')
 
-#tentang_manager = TentangManager(DAO)
+tentang_manager = TentangManager(DAO)
 
 @tentang_view.route('/', methods=['GET'])
 def tentang():
