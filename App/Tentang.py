6344d4dff5db0d49cec3ce74824c91356a78486c
esafm24 <esa.firmansyah@gmail.com>
2025-06-21 18:26:12 +0700

    ok

--- App/Tentang.py
+++ App/Tentang.py
@@ -2,6 +2,5 @@ class Tentang():
     nama = ""
 
     def __init__(self, TentangDAO):
-		self.dao = TentangDAO
-		self.sess_key = "user" # session key
+        self.dao = TentangDAO
 
--- Controllers/TentangManager.py
+++ Controllers/TentangManager.py
@@ -2,11 +2,6 @@ from App.Tentang import Tentang
 
 class TentangManager():
     def __init__(self, DAO):
-	    self.user = Tentang(DAO.db.user)
-	    self.book = DAO.db.book
-		self.dao = self.user.dao
+        self.dao = self.user.dao
 		
-    def list(self):
-		user_list = self.dao.list()
-
-		return user_list
+    
