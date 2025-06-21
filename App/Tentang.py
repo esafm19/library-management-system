57c12f7cc5816cf7e60d23f805b7dcd9c160312e
esafm24 <esa.firmansyah@gmail.com>
2025-06-21 18:46:06 +0700

    ok

--- App/Tentang.py
+++ App/Tentang.py
@@ -1,6 +1,5 @@
 class Tentang():
     nama = ""
 
-    def __init__(self, TentangDAO):
-        self.dao = TentangDAO
+
 
--- Controllers/TentangManager.py
+++ Controllers/TentangManager.py
@@ -1,6 +1,4 @@
 from App.Tentang import Tentang
 
 class TentangManager():
-    def __init__(self, DAO):
-		self.tentang = Tentang(DAO.db.user)
-		self.dao = self.user.dao
+   
