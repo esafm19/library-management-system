cf1fa5a2817fc0e6160aa2ce64402bd57d4623a5
esafm24 <esa.firmansyah@gmail.com>
2025-06-21 18:43:11 +0700

    ok

--- routes/tentang.py
+++ routes/tentang.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, g, escape, session, redirect, render_template, request, jsonify, Response, flash
-from app import DAO
+#from app import DAO
 from Misc.functions import *
 
 from Controllers.TentangManager import TentangManager
 
 tentang_view = Blueprint('tentang_routes', __name__, template_folder='/templates')
 
-tentang_manager = TentangManager(DAO)
+#tentang_manager = TentangManager(DAO)
 
 @tentang_view.route('/', methods=['GET'])
-def tentang():
+def home():
     return render_template('tentang.html')
