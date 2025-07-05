5d31fe13ef94f801c722ed650bc94e6508e6527c
esafm24 <esa.firmansyah@gmail.com>
2025-07-05 12:53:50 +0700

    ok

--- routes/tentang.py
+++ routes/tentang.py
@@ -3,6 +3,7 @@ from flask import Blueprint, g, escape, session, redirect, render_template, requ
 from Misc.functions import *
 
 #from Controllers.TentangManager import TentangManager
+# test
 
 tentang_view = Blueprint('tentang_routes', __name__, template_folder='/templates')
 
